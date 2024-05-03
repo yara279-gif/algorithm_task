e3df789ce1fcdd69c5f6d404fd9d46d75f825fcc
yara harby <yaraharby4@gmail.com>
2024-05-03 05:55:02 +0300

    non_recursion

---
+++ non_recursion/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int countDistinct(int A[], int n) {
+    int distinctCount = 0;
+
+    for (int i = 0; i < n; ++i) {
+        bool isDistinct = true;
+        // Check if the current element is distinct from previous elements
+        for (int j = 0; j < i; ++j) {
+            if (A[i] == A[j]) {
+                isDistinct = false;
+                break;
+            }
+        }
+        // If the current element is distinct, increment the distinct count
+        if (isDistinct)
+            distinctCount++;
+    }
+
+    return distinctCount;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int arr [n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+
+    cout << "Number of distinct values: " << countDistinct(arr, n) << endl;
+return 0;
+}
