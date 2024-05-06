f69c3caa74b709378c508b26b7a248229e16b84c
micheal965 <micheal.magdy69@yahoo.com>
2024-05-06 22:13:20 +0300

    task completed

--- rec_task_algo/main.cpp
+++ rec_task_algo/main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 
 using namespace std;
+
 void heapify(int arr[],int n,int i);
 void build_max_heap(int arr[],int n);
 void heap_sort(int arr[],int n);
 int count_distinct(int arr[],int i,int variable,int c,int n);
 int distinct (int arr [],int n);
+
 int main()
 {
     int n;
+    printf("Enter size of array:");
     cin>>n;
     int arr[n];
-    for (int i=0;i<n;i++){
-cin>>arr[i];
-    }
+    printf("Enter the items you want to know the distinct between them:");
+for (int i=0;i<n;i++){
+      cin>>arr[i];
+}
+
 int count_dist =distinct(arr,n);
-cout<<count_dist;
+cout<<"Number of  distinct is: "<<count_dist;
 
     return 0;
 }
@@ -38,6 +43,7 @@ for(int i=n/2-1;i>=0;i--){
     heapify(arr,n,i);
 }
 }
+
 //sort array
 void heap_sort(int arr[],int n){
 build_max_heap(arr,n);
@@ -52,7 +58,8 @@ int count_distinct(int arr[],int i,int variable,int c,int n){
 
 if(i>n-1){
     return c;
-}else{
+}
+else{
 if(variable!=arr[i]){
     c++;
     variable=arr[i];}
